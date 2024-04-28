b3edc656a6e14b50a5d2fcb20699bbeab974d759
Sky Zhao <skyzhao04@gmail.com>
2024-04-27 21:46:21 -0700

    sky changes
    added url for all the emotions

---
+++ health-scanner/lib/emotion_pages/sad_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+class SadPage extends StatelessWidget {
+  SadPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Emotion Page'),
+          elevation: 0,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SadLinksPage())
+                  );
+                },
+                child: Text('Capture Emotion'),
+              ),
+            ],
+          ),
+        ));
+  }
+}
+
+class SadLinksPage extends StatelessWidget {
+  const SadLinksPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sad Links'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text('Link 1'),
+            onTap: () {
+              // Link 1
+              _launchURL('https://example.com/link1');
+            },
+          ),
+          ListTile(
+            title: Text('Link 2'),
+            onTap: () {
+              // Link 2
+              _launchURL('https://example.com/link2');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Function to launch URL
+  void _launchURL(String url) {
+    // Add logic to launch the URL
+    print('Launching URL: $url');
+  }
+}
