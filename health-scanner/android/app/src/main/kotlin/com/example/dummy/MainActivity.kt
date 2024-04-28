43becab59e0113c126d564a3005829a51f0d8313
Sky Zhao <skyzhao04@gmail.com>
2024-04-27 20:15:38 -0700

    ch
    push

---
+++ health-scanner/android/app/src/main/kotlin/com/example/dummy/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dummy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ health-scanner/lib/util/button_widget.dart
@@ -0,0 +1,10 @@
+/*
+import 'package:flutter/material.dart';
+
+class ButtonWidget extends StatefulWidget {
+  const ButtonWidget({super.key});
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}*/
